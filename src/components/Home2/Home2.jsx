6add8d32f7b3d406a07ae92f6093fa7c5278a3cf
Shashank Gurunaga <shashankgurunaga@gmail.com>
2025-06-03 09:39:01 +0530

    New changes

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -11,14 +11,14 @@ const Home2 = () => {
   return (
     <section className="home2-section" id="about"> 
       <Container> 
-        <Row className="justify-content-center text-center my-5"> 
+        <Row className="justify-content-center text-center my-2"> 
           <Col md={10} lg={8} className="home2-content"> 
            
-            <h2 className="display-7 fw-bold mb-3">
+            <h2 className="display-7 fw-bold mb-1">
               I am currently  working in the following domains: <span className="purple">Generative AI</span>, <span className="purple">Machine Learning</span>, and <span className="purple">Deep Learning</span>.
             </h2>
            
-            <p className="lead mb-4">
+            <p className="lead mb-3">
               I develop  backend as well as frontend of <span className="purple">web and mobile apps</span> as well.
             </p>
             
