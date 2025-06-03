c450220d19cbedb5c18ea58232162473dd6ef0a5
Shashank Gurunaga <shashankgurunaga@gmail.com>
2025-06-03 09:55:16 +0530

    Made new changes

--- src/components/Home2/Home2.jsx
+++ src/components/Home2/Home2.jsx
@@ -1,41 +1,38 @@
 import React from "react";
-import Container from "react-bootstrap/Container"; 
-import Row from "react-bootstrap/Row";         
-import Col from "react-bootstrap/Col";           
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Links from "../Links/Links";
-
-
-import './Home2.css'; 
+import './Home2.css';
 
 const Home2 = () => {
   return (
-    <section className="home2-section" id="about"> 
-      <Container> 
-        <Row className="justify-content-center text-center my-2"> 
-          <Col md={10} lg={8} className="home2-content"> 
-           
-            <h2 className="display-7 fw-bold mb-1">
-              I am currently  working in the following domains: <span className="purple">Generative AI</span>, <span className="purple">Machine Learning</span>, and <span className="purple">Deep Learning</span>.
+    <section className="home2-section" id="about">
+      <Container>
+        <Row className="justify-content-center text-center my-3"> {/* reduced vertical margin */}
+          <Col md={10} lg={8} className="home2-content">
+            <h2 className="display-7 fw-bold mb-2">
+              I am currently working in the following domains: <span className="purple">Generative AI</span>, <span className="purple">Machine Learning</span>, and <span className="purple">Deep Learning</span>.
             </h2>
-           
-            <p className="lead mb-3">
-              I develop  backend as well as frontend of <span className="purple">web and mobile apps</span> as well.
+
+            <p className="title1">
+              I work as a <span className="purple"><strong>Full-Stack developer</strong></span>.
             </p>
-            
-            <h2 className="languages-heading mb-3">Languages:</h2>
-            <p className="languages-list">
-              Java, Python, C#, JavaScript,C++
+
+            <h2 className="languages-heading mb-1">Languages:</h2>
+            <p className="languages-list mb-2">
+              Java, Python, C#, JavaScript, C++
             </p>
           </Col>
-         
         </Row>
-        <Row>
-        <Col>
-          <h1> CONNECT VIA:</h1>
+
+        <Row className="text-center mt-3"> {/* reduced margin-top */}
+          <Col>
+            <h3 className="connect-heading">CONNECT VIA:</h3>
           </Col>
         </Row>
       </Container>
-      <Links/>
+      <Links />
     </section>
   );
 };
